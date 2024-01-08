ebbecd1f2ada0599ba1350eb20107db3d17d4937
TR-Mohd <85482881+TR-Mohd@users.noreply.github.com>
2024-01-08 15:47:39 +0300

    Delete PMI-PESTLE directory

--- PMI-PESTLE/JS/script.js
+++
@@ -1,8 +0,0 @@
-const optionMenu = document.querySelector("#sideBar"),
-      selectBtn = optionMenu.querySelector(".select_btn"),
-      options = optionMenu.querySelector(".options")
-
-selectBtn.addEventListener('click', () => optionMenu.classList.toggle('active'))
-
-const riskAssessment = optionMenu.querySelector("#risk_assessment");
-riskAssessment.addEventListener('click', ()=> options.classList.toggle('show'))
